b006cf44a8ef61d381919460fd39410b815fe973
Lucas Feliciano <162584840+Felici4no@users.noreply.github.com>
2024-09-01 15:43:51 -0300

    creating the project base
    Criando a base do Portifolio.

---
+++ src/scripts/functions.js
@@ -0,0 +1,184 @@
+const certificates = [
+    {
+        title: "Fundamentos de decodificação",
+        link: "https://drive.google.com/file/d/1CjHQXB6N3uenrEXlOVjuOWl78teKNCeF/view?usp=sharing",
+        date: "set de 2023"
+    },
+    {
+        title: "Arduino Irrigação Inteligente",
+        link: "https://drive.google.com/file/d/1aw7vQWxo99b8nVSOImakSWyghLY6-Ip3/view?usp=drive_link",
+        date: "jul de 2023"
+    },
+    {
+        title: "Lógica de Programação",
+        link: "https://drive.google.com/file/d/10-FV44cO2_VKbZbAMTg04-C5EHFr5MoB/view?usp=sharing",
+        date: "fev de 2024"
+    },
+    {
+        title: "Design thinking",
+        link: "https://drive.google.com/file/d/1-y39qla5NYmvVLUCREK1ffEBUJItdK3x/view?usp=drive_link",
+        date: "mar de 2024"
+    },
+    {
+        title: "Learn to Program",
+        link: "https://drive.google.com/file/d/1-ybtLt-lyn2Irnpw0EydNzDbE5HNcV09/view?usp=drive_link",
+        date: "jun de 2024"
+    },
+    {
+        title: "Lógica de programação: mergulhe em programação com JavaScript",
+        link: "https://drive.google.com/file/d/1-lW5Sq0uNQnBObdG9wTToVTN3HWHn6h8/view?usp=sharing",
+        date: "jun de 2024"
+    },
+    {
+        title: "Algoritmos: aprenda a programar",
+        link: "https://drive.google.com/file/d/10hp84qwW4bouqBNIt0CVVdDlt6u1MG2k/view?usp=sharing",
+        date: "jul de 2024"
+    },
+    {
+        title: "Lógica de programação: explore funções e listas",
+        link: "https://drive.google.com/file/d/1-kC5bmfIeUeeOKZEyKv283vfVY-aqE4p/view?usp=sharing",
+        date: "jul de 2024"
+    },
+    {
+        title: "Git e GitHub: compartilhando e colaborando em projetos",
+        link: "https://drive.google.com/file/d/1-g07hUkT2RgIa2Jh4uFs9a_spxhZxYWy/view?usp=sharing",
+        date: "jul de 2024"
+    },
+    {
+        title: "Psicologia positiva: liderança autêntica a partir de forças pessoais",
+        link: "https://drive.google.com/file/d/1-sNYEy_JPhcuUJj9mNnLqMO55dPQzABX/view?usp=sharing",
+        date: "jul de 2024"
+    },
+    {
+        title: "Mentalidade de crescimento: potencializando sua carreira",
+        link: "https://drive.google.com/file/d/1-qIFZ5CykucKG0Fo71WAYW7bSxcFo2uP/view?usp=sharing",
+        date: "jul de 2024"
+    },
+    {
+        title: "Lógica de programação: praticando com desafios",
+        link: "https://drive.google.com/file/d/1-q4Sa8i5uAV7GOBQil-JB7WKWj04uuul/view?usp=sharing",
+        date: "jul de 2024"
+    },
+    {
+        title: "Inteligência emocional: aumente seu potencial de liderança",
+        link: "https://drive.google.com/file/d/1-j4dAR0DXlmuPlH3LV5lYQWya-pcdD3X/view?usp=sharing",
+        date: "jul de 2024"
+    },
+    {
+        title: "Integridade corporativa: tomada de decisões éticas em ambientes empresariais",
+        link: "https://drive.google.com/file/d/1-gJfL-HU3u0ldiD0LLjbP87YokYAMlgK/view?usp=sharing",
+        date: "jul de 2024"
+    },
+    {
+        title: "Gerenciamento de estresse: otimize sua liderança",
+        link: "https://drive.google.com/file/d/1-cRp1AaLWnvre90lucRGZ0hp8L1SMj60/view?usp=sharing",
+        date: "jul de 2024"
+    },
+    {
+        title: "JavaScript",
+        link: "https://drive.google.com/file/d/10fz4D-yKUsMxcdDFYdZ_V2y3dA6ZPLhS/view?usp=sharing",
+        date: "jul de 2024"
+    },
+    {
+        title: "C: conhecendo a Linguagem das Linguagens",
+        link: "https://drive.google.com/file/d/10vOTBzD57D3c7ugU97z5oLkVTsXIvBuh/view?usp=sharing",
+        date: "jul de 2024"
+    },
+    {
+        title: "Formação A partir do zero: iniciante em programação",
+        link: "https://drive.google.com/file/d/1-u0QV_EnXvv0zdfA8i0UZKwxHyBBU_s0/view?usp=sharing",
+        date: "jul de 2024"
+    },
+    {
+        title: "Formação Autoliderança",
+        link: "https://drive.google.com/file/d/1-ujr48m_iSWEIe-h-7Hd3WFG54vx_24y/view?usp=sharing",
+        date: "jul de 2024"
+    },
+    {
+        title: "Startupismo: comece do jeito certo",
+        link: "https://drive.google.com/file/d/118UAuXcFH1z7X88FT4ldHC5pPWwI_ZU3/view?usp=sharing",
+        date: "ago de 2024"
+    },
+    {
+        title: "Design: entenda e conheça a área",
+        link: "https://drive.google.com/file/d/10x1r4SmXUq7SP7Pti_vwhFB8DqS-c0e1/view?usp=sharing",
+        date: "ago de 2024"
+    },
+    {
+        title: "Git e GitHub: dominando controle de versão de código",
+        link: "https://drive.google.com/file/d/11oEpVrRcbhddAlaVzkFtSLFcv90FJTVF/view?usp=sharing",
+        date: "ago de 2024"
+    },
+    {
+        title: "C: avançando na linguagem",
+        link: "https://drive.google.com/file/d/11yDXLf3Rds304d6y3jx1PcBdTCHCz4oL/view?usp=sharing",
+        date: "ago de 2024"
+    },
+    {
+        title: "Gestão de produtos digitais: Produto vs. Projeto",
+        link: "https://drive.google.com/file/d/12EOKs5iveN18miJCoG6eWvulgiXJ9Tdl/view?usp=sharing",
+        date: "ago de 2024"
+    },
+    {
+        title: "JavaScript e HTML: desenvolva um jogo e pratique lógica de programação",
+        link: "https://drive.google.com/file/d/1231Z9IgKUKCYGb_jRAN_ZZjDq9SgeA4J/view?usp=sharing",
+        date: "ago de 2024"
+    },
+    {
+        title: "Em andamento",
+        link: "#",
+        date: ""
+    }
+];
+
+function renderCertificates() {
+    const certificatesList = document.getElementById("certificates-list");
+
+    certificates.forEach(cert => {
+        const listItem = document.createElement("li");
+
+        listItem.innerHTML = `
+            <div class="content">
+                <h3>${cert.title}</h3>
+                <p><a href="${cert.link}">Certificado</a></p>
+            </div>
+            <div class="date">
+                <h4>${cert.date}</h4>
+            </div>
+        `;
+
+        certificatesList.appendChild(listItem);
+    });
+}
+
+// Chame a função para renderizar os certificados quando a página carregar
+window.onload = renderCertificates;
+
+
+
+const slider = document.querySelector('.slider');
+const slideWidth = slider.clientWidth;
+
+// Array com os links das imagens
+const images = [
+    'https://media.licdn.com/dms/image/v2/D4D22AQHQmIP0lVbMtg/feedshare-shrink_1280/feedshare-shrink_1280/0/1714746712552?e=1727913600&v=beta&t=ep8MHApnBzLYwWgctcIMnkQ4nG6ErdVv6KVQFkABzck',
+    'https://media.licdn.com/dms/image/v2/D4D22AQGnudpziDKRJA/feedshare-shrink_2048_1536/feedshare-shrink_2048_1536/0/1714746712934?e=1727913600&v=beta&t=EQblSOQBqj-KufK0DvMRTvRQPYz2HHvSk4YZlkXWFFM',
+    'https://media.licdn.com/dms/image/v2/D4D22AQH9giLY2MpA4A/feedshare-shrink_2048_1536/feedshare-shrink_2048_1536/0/1714746712585?e=1727913600&v=beta&t=qFMY7wVB2ge5RbtvQ0wm7A2uvGGEJo8UhaLqMkDEPO4'
+];
+
+// Função para criar as slides
+function createSlides() {
+    images.forEach(image => {
+        const slide = document.createElement('div');
+        slide.classList.add('slide');
+        const img = document.createElement('img');
+        img.src = image;
+        slide.appendChild(img);
+        slider.appendChild(slide);
+    });
+}
+
+// Chamar a função para criar as slides
+createSlides();
+
+// ... (resto do código para a lógica do carrossel)
