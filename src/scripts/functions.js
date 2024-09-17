1f2c18c134d4f8d7a417d01e0cb1b436f4bb00cf
Lucas Feliciano <lucas.for.study.42@gmail.com>
2024-09-17 18:12:44 -0300

    sobre mim

--- src/scripts/functions.js
+++ src/scripts/functions.js
@@ -7,3 +7,24 @@ document.querySelectorAll('.links a').forEach(link => {
         link.style.transform = 'scale(1)';
     });
 });
+
+const modal = document.getElementById("myModal");
+const openModalBtn = document.getElementById("openModalBtn");
+const closeBtn = document.getElementsByClassName("close")[0];
+
+//clicar no botão, abre o modal
+openModalBtn.onclick = function() {
+    modal.style.display = "block";
+}
+
+//clicar no X, fecha o modal
+closeBtn.onclick = function() {
+    modal.style.display = "none";
+}
+
+//clicar fora do modal, também fecha
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
